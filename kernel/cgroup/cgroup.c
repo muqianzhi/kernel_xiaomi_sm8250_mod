b14656d37a04778428e99bd82765f54d59a82bd3
muqianzhi <1643996049@qq.com>
2026-07-10 11:36:38 +0800

    Move cgroup link creation to a different location

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -3138,11 +3138,6 @@ static int cgroup_apply_control_enable(struct cgroup *cgrp)
 		}
 	}
 
-	if (cft->ss && (cgrp->root->flags & CGRP_ROOT_NOPREFIX) && !(cft->flags & CFTYPE_NO_PREFIX)) {
-				snprintf(name, CGROUP_FILE_NAME_MAX, "%s.%s", cft->ss->name, cft->name);
-				kernfs_create_link(cgrp->kn, name, kn);
-	}
-
 	return 0;
 }
 
@@ -3953,6 +3948,11 @@ static int cgroup_add_file(struct cgroup_subsys_state *css, struct cgroup *cgrp,
 		spin_lock_irq(&cgroup_file_kn_lock);
 		cfile->kn = kn;
 		spin_unlock_irq(&cgroup_file_kn_lock);
+	}
+
+	if (cft->ss && (cgrp->root->flags & CGRP_ROOT_NOPREFIX) && !(cft->flags & CFTYPE_NO_PREFIX)) {
+		snprintf(name, CGROUP_FILE_NAME_MAX, "%s.%s", cft->ss->name, cft->name);
+		kernfs_create_link(cgrp->kn, name, kn);
 	}
 	
 	return 0;
